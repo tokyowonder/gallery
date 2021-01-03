039bf9d0e6fe6f4c8a0ebad9b89319fc6cf2212b
Barry <barry@netlands.com>
2021-01-03 12:26:25 +0900

    Initial commit

---
+++ scripts/store.js
@@ -0,0 +1,12 @@
+/*
+<form id="purchase_form" method="post">
+    <div class="itemPrice">
+        <h3>
+            <div>¥ 25,000</div>
+        </h3>
+    </div>
+    <div id="purchaseButton" class="purchaseButton">
+        <button type="submit" class="purchaseButton">Add to cart</button>
+    </div>
+</form>
+*/
